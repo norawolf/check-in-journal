cdd1b6a8e9142ff14855fac472bd1346c02fd306
norawolf <noraevogt@gmail.com>
2019-03-28 21:41:55 -0400

    fix current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 require './config/environment'
+require 'securerandom'
 
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+
+    enable :sessions
+    set :session_secret, "yarrowyarrowyarrow"
+    # this recommended line from sinatra docs wasn't working, did not save added
+    # session variables across routes?
+    # set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
   end
 
   get "/" do
@@ -13,10 +20,10 @@ class ApplicationController < Sinatra::Base
 
   # define helpers for logged_in and current_user here
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    User.find_by(id: session[:user_id])
   end
 
   def logged_in?
-     !!current_user
+     !!session[:user_id]
   end
 end
