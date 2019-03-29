29e1ab79563505b58b9769a2330dd2b85651166e
norawolf <noraevogt@gmail.com>
2019-03-28 20:31:18 -0400

    add application controller helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,11 @@ class ApplicationController < Sinatra::Base
   end
 
   # define helpers for logged_in and current_user here
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 
+  def logged_in?
+     !!current_user
+  end
 end
