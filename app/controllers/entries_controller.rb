7de70af3445f6d82a7b7e97c02a3055ce64ad8e2
norawolf <noraevogt@gmail.com>
2019-03-30 22:35:12 -0400

    update /entries get and post routes

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,12 @@
 class EntriesController < ApplicationController
 
   get '/entries/new' do
+    @moods = Mood.all
+
     erb :'/entries/new'
+  end
+
+  post '/entries' do
+    binding.pry
   end
 end
