933ee3834ceb558e6e419601b8667aa9070c6365
norawolf <noraevogt@gmail.com>
2019-03-30 21:56:23 -0400

    scrape external site for Mood seeds

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+require 'open-uri'
+MOODS = "https://www.vocabulary.com/lists/535865"
+
+def get_page(url)
+  Nokogiri::HTML(open(url))
+end
+
+all_moods = []
+page = get_page(MOODS)
+nodeset = page.css("a.word.dynamictext")
+
+nodeset.each do |node|
+  all_moods << {name: node.text}
+end
+
+all_moods.each do |hash|
+  Mood.find_or_create_by(hash)
+end
