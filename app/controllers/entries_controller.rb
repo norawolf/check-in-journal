6db7dea25e4bdcb2e7cc3bd3ebd5e9f1a340b76e
norawolf <noraevogt@gmail.com>
2019-03-31 18:41:09 -0400

    add logic to create a new entry to post route

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -7,6 +7,19 @@ class EntriesController < ApplicationController
   end
 
   post '/entries' do
-    binding.pry
+    @entry = Entry.new(date: params[:entry][:date], note: params[:entry][:note],
+      user_id: session[:user_id])
+
+    params[:entry][:moods].each do |mood|
+      @entry.moods << Mood.find_or_create_by(name: mood)
+    end
+
+    params[:entry][:activities].each do |activity|
+      @entry.activities << Activity.find_or_create_by(name: activity)
+    end
+
+    @entry.save
   end
+
+  
 end
