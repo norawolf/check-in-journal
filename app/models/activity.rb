a0996aa2bc3e8b7aa35e5fec6983d2f26fec70ef
norawolf <noraevogt@gmail.com>
2019-04-01 22:04:17 -0400

    add validation for name attribute

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,6 @@
 class Activity < ActiveRecord::Base
   has_many :activities_entries
   has_many :entries, through: :activities_entries
+
+  validates :name, presence: true
 end
--- app/models/mood.rb
+++ app/models/mood.rb
@@ -1,4 +1,6 @@
 class Mood < ActiveRecord::Base
   has_many :moods_entries
   has_many :moods, through: :moods_entries
+
+  validates :name, presence: true
 end
