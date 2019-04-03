21413c9ff92e43593120836532beff8941b4faeb
norawolf <noraevogt@gmail.com>
2019-04-03 15:33:38 -0400

    memoize current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < Sinatra::Base
 
   # define helpers for logged_in and current_user here
   def current_user
-    User.find_by(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def logged_in?
