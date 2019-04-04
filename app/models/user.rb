60f705fe93597645966bf06dc4241eada263f2f8
norawolf <noraevogt@gmail.com>
2019-04-04 10:59:44 -0400

    update spacing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
   has_many :entries
 
-  validates :username, :email, :password, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 
   has_secure_password
 end
