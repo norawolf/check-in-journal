6ffc0f516958266f1342df90707d612d9fd8bc11
norawolf <noraevogt@gmail.com>
2019-04-04 22:52:07 -0400

    remove redundant if statment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,6 @@ class ApplicationController < Sinatra::Base
   end
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 end
