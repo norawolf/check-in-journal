8e5cfcbf8332a216f47875663533fa4d7d96a3df
norawolf <noraevogt@gmail.com>
2019-04-16 13:38:51 -0400

    fix has_many relationship

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -1,6 +1,7 @@
 class Mood < ActiveRecord::Base
   has_many :moods_entries
-  has_many :moods, through: :moods_entries
+  has_many :entries, through: :moods_entries
 
   validates :name, presence: true
+
 end
