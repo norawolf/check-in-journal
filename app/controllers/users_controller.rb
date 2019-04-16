e33f7ed49dff7e6a96d19546b02638230de763dc
norawolf <noraevogt@gmail.com>
2019-04-16 13:59:10 -0400

    refactor signup action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,7 @@ class UsersController < ApplicationController
   post '/signup' do
     @user = User.new(params)
 
-    if @user.valid?
-      @user.save
+    if @user.save
       session[:user_id] = @user.id
       redirect '/dashboard'
     else
